b167749503c4d71d97a8201623548474673b5351
Jourdan Harvey <jourdan.harvey@gmail.com>
2021-01-27 21:21:07 -0800

    Comparisons component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import './App.css';
-import React, { useEffect } from "react";
 import { Route, Switch } from 'react-router';
-import { connect, ConnectedProps } from 'react-redux';
 
 import * as routes from './constants/routes';
 
 import Create from './components/create/Create';
 import SignIn from './components/signin/SignIn';
 import Comparison from './components/comparison/Comparison';
-import { RootState } from './redux/reducers';
-// import { FirebaseUser } from './services/firebase';
-
+import Comparisons from './components/comparisons/Comparisons';
 
 function App() {
   return (    
     <Switch>
       <Route exact path={routes.HOME} component={Create} />
+      <Route exact path={routes.CREATE} component={Create} />
+      <Route exact path={routes.COMPARISONS} component={Comparisons} />
       <Route exact path={routes.COMPARISON} component={Comparison} />
       <Route exact path={routes.SIGNIN} component={SignIn} />
       <Route>404</Route>
